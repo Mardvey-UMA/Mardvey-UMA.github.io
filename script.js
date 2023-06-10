dea50584a1ea048e8ba07c8547cd91cb55cf80a9
Mattew Razinov <83042309+Mardvey-UMA@users.noreply.github.com>
2023-06-10 07:45:23 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,12 @@
-/*
-const buttons = document.querySelectorAll('.button');
-
-buttons.forEach(button => {
+const prices = document.querySelectorAll('.price');
+prices.forEach(price => {
+  price.textContent += ' (100 neocoins)';
+});
+const buyButtons = document.querySelectorAll('.buy-button');
+buyButtons.forEach(button => {
   button.addEventListener('click', () => {
-    console.log(`Нажата кнопка ${button.id}`);
+    const item = button.parentNode;
+    const itemName = item.querySelector('h2').textContent;
+    alert(`Вы успешно купили "${itemName}"!`);
   });
 });
-*/
-let tg = window.Telegram.WebApp;
-tg.expand();
-tg.MainButton.textColor = "#FFFFFF";
-tg.MainButton.color = "#2cab37"
-let item = "";
-
-let btn1 = document.getElementById("button1");
-btn1.addEventListener("click", function(){
-  item = "1";
-  if (tg.MainButton.isVisible){
-    item = "1";
-    tg.MainButton.hide();
-  }
-  /*else{
-    tg.MainButton.setText("Вы выбрали задачу 1");
-    item = "1";
-    MainButton.show();
-  }*/else{
-    tg.sendData(item);
-  }
-});
-
-Telegram.WebApp.onEvent("mainButtonClicked", function(){
-  tg.sendData(item);
-});
-/*
-Telegram.WebApp.onEvent("mainButtonClicked", function(){
-  tg.sendData(item);
-});
-*/
-let usercard = document.getElementById("usercard");
-let p = document.createElement("p");
-p.innerText = `${tg.initDataUnsafe.user.first_name} ${tg.initDataUnsafe.user.last_name}`
-
-usercard.appendChild(p);
